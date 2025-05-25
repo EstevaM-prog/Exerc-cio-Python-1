a10bfe0ec7a8d25e3a3bfa04fad4f285857a9656
Felipe Estevam <estevamx9@gmail.com>
2025-05-25 19:03:10 -0300

    Update aula03.py
    arrumando

--- aula03.py
+++ aula03.py
@@ -12,27 +12,31 @@ def exibir_nome_do_programa():
 """)
 
 def exibir_opcao():
-    print("1. Cadastrar restaurante")
-    print("2. Listar restaurante")
-    print("3. Ativar restaurante")
-    print("4. Sair\n")
+    print ("1. Cadastrar restaurante")
+    print ("2. Listar restaurante")
+    print ("3. Ativar restaurante")
+    print ("4. Sair\n")
 
 def finalizar_app():
-    os.system('cls' if os.name == 'nt' else 'clear')
+    os.system('cls' if os.name == 'nt' else 'clear')  # mais compatível com diferentes SOs
     print("Finalizando o app\n")
 
 def escolher_opcao():
-    while True:
-        try:
-            opcao_escolhida = int(input('Escolha uma opção: '))
-            if opcao_escolhida == 1:
-                print("Cadastrar restaurante")
-            elif opcao_escolhida == 2:
-                print("Listar restaurantes")
-            elif opcao_escolhida == 3:
-                print("Ativar restaurante")
-            elif opcao_escolhida == 4:
-                finalizar_app()
-                break
-            else:
-                print("Opçã
+    opcao_escolhida = int(input('Escolha uma opção: '))
+
+if opcao_escolhida == 1:
+        print("Cadastrar restaurante")
+    elif opcao_escolhida == 2:
+        print("Listar restaurantes")
+    elif opcao_escolhida == 3:
+        print("Ativar restaurante")
+    else:
+        finalizar_app()
+
+def main():
+    exibir_nome_do_programa()
+    exibir_opcoes()
+    escolher_opcao()
+
+if __name__ == '__main__':
+    main()
